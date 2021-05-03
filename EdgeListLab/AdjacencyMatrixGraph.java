2c40c87dbedf9ea8aeb92ca4d9ac613e02bffb5b
l_ladios <49729440+LeiaCarmenLadios@users.noreply.github.com>
2021-05-02 22:21:46 -0500

    Add files via upload

--- EdgeListLab/AdjacencyMatrixGraph.java
+++ EdgeListLab/AdjacencyMatrixGraph.java
@@ -20,9 +20,16 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E>{
 	
 	private class InEdge<E> implements Edge<E>{
 		private E element;
+		private InVertex<V> to;
+		private InVertex<V> from; 
 		
 		public InEdge(E element) {
 			this.element = element;
+		}
+		
+		public InEdge(InVertex<V> to, InVertex<V> from) {
+			this.to = to;
+			this.from = from;
 		}
 		
 		@Override
@@ -31,29 +38,50 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E>{
 			return element;
 		}
 		
+		public InVertex<V> getToVertex(){
+			return to; 
+		}
+		
+		public InVertex<V> getFromVertex(){
+			return from;
+		}
+		
 	} //end of nested InVertex class
 	
 	private Integer[][] adjMat;
+	private InVertex<V>[] vertices;
+	private InEdge<E>[] edges;
 	
 	public AdjacencyMatrixGraph(V[] vertices, V[][] vertexPairs) {
-		
+		this.vertices = new InVertex[vertices.length];
+		this.edges = new InEdge[vertexPairs.length];
 		adjMat = new Integer[vertices.length][vertices.length];
-		
 		for(int i = 0; i < vertices.length; i++) {
-			for(int j = 0; j < vertexPairs[0].length; j++) {
-				if(vertices[i] == vertexPairs[j][0] ) {
-					adjMat[i][j] = 1;
-					System.out.print(vertices[i] + " " + vertexPairs[j][0] + " ");
-					System.out.print(adjMat[i][j] + " \n");
-				}
-			}
+			InVertex<V> vert = new InVertex<V>(vertices[i]);
+			this.vertices[i] = vert;
+			
+			System.out.println(this.vertices[i].getElement());
 		}
+		
+		for(int j = 0; j < vertexPairs.length; j++) {
+			InVertex<V> to = new InVertex<V>(vertexPairs[j][0]);
+			InVertex<V> from = new InVertex<V>(vertexPairs[j][1]);
+			InEdge<E> edge = new InEdge<E>(to, from);
+			
+			System.out.println("edge:\t v1: " + to.getElement() + " v2: " + from.getElement());
+			this.edges[j] = edge;
+		}
+		
+		
+	}
+	
+	private void formMatrix() {
 		
 	}
 	@Override
 	public int numVertices() {
 		// TODO Auto-generated method stub
-		return 0;
+		return vertices.length;
 	}
 
 	@Override
@@ -65,7 +93,7 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E>{
 	@Override
 	public int numEdges() {
 		// TODO Auto-generated method stub
-		return 0;
+		return edges.length;
 	}
 
 	@Override
@@ -77,6 +105,12 @@ public class AdjacencyMatrixGraph<V, E> implements Graph<V, E>{
 	@Override
 	public Edge<E> getEdge(Vertex<V> vertOne, Vertex<V> vertTwo) {
 		// TODO Auto-generated method stub
+		for(int i = 0; i < edges.length; i++) {
+			if(edges[i].getToVertex().getElement() == vertOne.getElement() &&
+			   edges[i].getFromVertex().getElement() == vertTwo.getElement()) {
+				return edges[i];
+			}
+		}
 		return null;
 	}
 
